76f9d65ec7a934a52f3effbfc35f27043698ae1f
XDRayLl <rayli0413@gmail.com>
2025-09-26 20:52:24 +0800

    TeamsPageModule

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,8 +1,12 @@
 import Nav from "../JSX/Nav"
+import TeamsCards from "../JSX/TeamsCards"
 
 function Teams(){
     return(
-        <Nav/>
+        <div>
+            <Nav/>
+            <TeamsCards value="Mcl" TeamsName={"McLaren"} DriverName1={"Oscar PIASTRI"} DriverName2={"Lando NORRIS"} Driverimg1={"https://media.formula1.com/image/upload/c_lfill,w_40/q_auto/d_common:f1:2025:fallback:driver:2025fallbackdriverright.webp/v1740000000/common/f1/2025/mclaren/oscpia01/2025mclarenoscpia01right.webp"} Driverimg2={"https://media.formula1.com/image/upload/c_lfill,w_40/q_auto/d_common:f1:2025:fallback:driver:2025fallbackdriverright.webp/v1740000000/common/f1/2025/mclaren/lannor01/2025mclarenlannor01right.webp"} Logosrc={"https://media.formula1.com/image/upload/c_lfill,w_48/q_auto/v1740000000/common/f1/2025/mclaren/2025mclarenlogowhite.webp"} Carimg={"https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common/f1/2025/mclaren/2025mclarencarright.webp"}/>
+        </div>
     );
 }
 
