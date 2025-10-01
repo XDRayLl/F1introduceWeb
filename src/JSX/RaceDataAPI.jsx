af463a42f35ecf0c14d702a34ccd951397b9f77e
XDRayLl <rayli0413@gmail.com>
2025-10-01 23:37:17 +0800

    顯示下場比賽名稱+幾天後

--- src/JSX/RaceDataAPI.jsx
+++ src/JSX/RaceDataAPI.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import moment from "moment"
+import moment from "moment";
 
 function Sessions() {
   const [data, setData] = useState(null); // 存API回傳資料
@@ -8,7 +8,9 @@ function Sessions() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch("https://api.jolpi.ca/ergast/f1/2025.json");
+        const response = await fetch(
+          "https://api.jolpi.ca/ergast/f1/2025.json"
+        );
         const json = await response.json();
         setData(json); // 更新 state
         console.log("xd: ", json);
@@ -37,7 +39,7 @@ function Sessions() {
       </div>
       <div>
         現在時間
-        <ShowTime />
+        <ShowTime races={data.MRData.RaceTable.Races} />
         {/* {setInterval(ShowTime,1000)} */}
       </div>
 
@@ -50,50 +52,23 @@ function judgeYears(Data) {
   // const Races = Data.filter((e) => e.session_type == "Race" && e.meeting_key>=1253);
   // const RaceDate = Races.map((e) => e.date_start);
   // const RaceConutry = Races.map((e) => e.country_name)
-  
+
   const Races = Data.MRData.RaceTable.Races;
-  const RaceDate = Races.map((e)=>e.date);
-  const RaceName = Races.map((e)=>e.raceName);
-  const RaceTime = Races.map((e)=>e.time);
- 
-  console.log("Race " ,Races)
-  console.log("RaceDate " ,RaceDate)
-  console.log("RaceName " ,RaceName)
-  console.log("RaceTime " ,RaceTime)
-  
-  let ThisyearsRaces=[]
-  for(var i=0; i<Races.length;i++){
-    let componment = RaceName[i] +" "+ RaceDate[i] +" "+ RaceTime[i]
-    ThisyearsRaces.push(componment)
+  const RaceDate = Races.map((e) => e.date);
+  const RaceName = Races.map((e) => e.raceName);
+  const RaceTime = Races.map((e) => e.time);
+
+  let ThisyearsRaces = [];
+  for (var i = 0; i < Races.length; i++) {
+    let componment = RaceName[i] + " " + RaceDate[i] + " " + RaceTime[i];
+    ThisyearsRaces.push(componment);
   }
-  
+
   return ThisyearsRaces;
 }
 
-function ShowTime() {
-  // var Today=new Date();
-  // var years = Today.getFullYear()
-  // var mouth = Today.getMonth()+1
-  // var date = Toady.getDate()
-  // var hour = Today.getHours()
-  // var min = Today.getMinutes()
-  // var sec = Today.getSeconds()
-  // console.log(NowTime)
-  // return(
-  //   <div>
-  //     <div>
-  //       現在時間<br/>
-  //       <h3>{years}年</h3>
-  //       <h3>{mouth}月</h3>
-  //       <h3>{date}日</h3>
-  //       <h3>{hour}點</h3>
-  //       <h3>{min}分</h3>
-  //       <h3>{sec}秒</h3>
-  //     </div>
-  //   </div>
-  // )
-
-  const [currentTime, setCurrentTime] = useState(new Date(),);
+function ShowTime({races}) {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -104,26 +79,50 @@ function ShowTime() {
     };
   }, []);
 
-  var date = currentTime.getFullYear()+"-"+(currentTime.getMonth()+1)+"-"+currentTime.getDate();
+  var date =
+    currentTime.getFullYear() +
+    "-" +
+    (currentTime.getMonth() + 1) +
+    "-" +
+    currentTime.getDate();
 
   // if(date !=)
 
-  var NowDay = moment(date)
-  var RaceDay = moment()
+  let Today = moment(date);
+
+  let upcomingRaceDate = races.map((race) => moment(race.date,"YYYY-MM-DD"))//moment(,顯示格式)(讓每個日期是變數race = monment(race.date)並形成一個陣列)
+  let upcomingRace = upcomingRaceDate.find((raceDate) => raceDate.isAfter(Today));
+
+  let upcomingRaceName
+   for(var i=0;i<upcomingRaceDate.length;i++){
+    if(upcomingRace == upcomingRaceDate[i]){
+      let AllRaceName = races.map((e)=>e.raceName)
+      upcomingRaceName = AllRaceName[i]
+    }
+   }
+
+  let Diffdays = upcomingRace.diff(Today, "days");
+  // console.log("相差天數", Diffdays);
 
   return (
     <div>
       <p>
-        {currentTime.getFullYear()}/{currentTime.getMonth()+1}/
+        {currentTime.getFullYear()}/{currentTime.getMonth() + 1}/
         {currentTime.getDate()}
       </p>
       <p>
         {currentTime.getHours()}:{currentTime.getMinutes()}:
         {currentTime.getSeconds()}
       </p>
+      <p>下場比賽{upcomingRaceName} 是 {Diffdays} 天後</p>
     </div>
   );
 }
+
+// function NextRaceTime(){
+//   let Today = moment()
+//   console.log(Today)
+// }
 
 function display() {}
 export default Sessions;
